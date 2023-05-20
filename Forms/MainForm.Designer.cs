dac37650eed19571b9e6ebab0b119d1c06a3148a
Евгений Михайлюков <mihaylyukov99@gmail.com>
2023-05-20 21:53:13 +0600

    добавил карту и инициализацию добавил вертикальный слайд для зума

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace InteractiveMap
+﻿namespace InteractiveMap.Forms
 {
     partial class MainForm
     {
@@ -28,51 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.MapView = new GMap.NET.WindowsForms.GMapControl();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.HeightBar = new System.Windows.Forms.TrackBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.HeightBar)).BeginInit();
             this.SuspendLayout();
             // 
-            // gMapControl1
+            // MapView
             // 
-            this.gMapControl1.Bearing = 0F;
-            this.gMapControl1.CanDragMap = true;
-            this.gMapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
-            this.gMapControl1.GrayScaleMode = false;
-            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
-            this.gMapControl1.LevelsKeepInMemmory = 5;
-            this.gMapControl1.Location = new System.Drawing.Point(0, 0);
-            this.gMapControl1.MarkersEnabled = true;
-            this.gMapControl1.MaxZoom = 2;
-            this.gMapControl1.MinZoom = 2;
-            this.gMapControl1.MouseWheelZoomEnabled = true;
-            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
-            this.gMapControl1.Name = "gMapControl1";
-            this.gMapControl1.NegativeMode = false;
-            this.gMapControl1.PolygonsEnabled = true;
-            this.gMapControl1.RetryLoadTile = 0;
-            this.gMapControl1.RoutesEnabled = true;
-            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
-            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
-            this.gMapControl1.ShowTileGridLines = false;
-            this.gMapControl1.Size = new System.Drawing.Size(1182, 953);
-            this.gMapControl1.TabIndex = 0;
-            this.gMapControl1.Zoom = 0D;
+            this.MapView.Bearing = 0F;
+            this.MapView.CanDragMap = true;
+            this.MapView.EmptyTileColor = System.Drawing.Color.Navy;
+            this.MapView.GrayScaleMode = false;
+            this.MapView.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.MapView.LevelsKeepInMemmory = 5;
+            this.MapView.Location = new System.Drawing.Point(23, 31);
+            this.MapView.MarkersEnabled = true;
+            this.MapView.MaxZoom = 2;
+            this.MapView.MinZoom = 2;
+            this.MapView.MouseWheelZoomEnabled = true;
+            this.MapView.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.MapView.Name = "MapView";
+            this.MapView.NegativeMode = false;
+            this.MapView.PolygonsEnabled = true;
+            this.MapView.RetryLoadTile = 0;
+            this.MapView.RoutesEnabled = true;
+            this.MapView.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.MapView.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.MapView.ShowTileGridLines = false;
+            this.MapView.Size = new System.Drawing.Size(979, 876);
+            this.MapView.TabIndex = 0;
+            this.MapView.Zoom = 0D;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.MapView);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1025, 929);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Map";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.HeightBar);
+            this.groupBox2.Location = new System.Drawing.Point(1043, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(327, 929);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Control";
+            // 
+            // HeightBar
+            // 
+            this.HeightBar.Location = new System.Drawing.Point(20, 31);
+            this.HeightBar.Maximum = 16;
+            this.HeightBar.Minimum = 2;
+            this.HeightBar.Name = "HeightBar";
+            this.HeightBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.HeightBar.Size = new System.Drawing.Size(56, 892);
+            this.HeightBar.TabIndex = 0;
+            this.HeightBar.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.HeightBar.Value = 2;
+            this.HeightBar.Scroll += new System.EventHandler(this.HeightBar_Scroll);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1182, 953);
-            this.Controls.Add(this.gMapControl1);
+            this.ClientSize = new System.Drawing.Size(1382, 953);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "MainForm";
+            this.ShowIcon = false;
             this.Text = "InteractiveMap";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.HeightBar)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private GMap.NET.WindowsForms.GMapControl MapView;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TrackBar HeightBar;
     }
 }
 
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -1,20 +1,38 @@
-﻿using System;
+﻿using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace InteractiveMap
+namespace InteractiveMap.Forms
 {
     public partial class MainForm : Form
     {
         public MainForm()
         {
             InitializeComponent();
+            InitializeMap();
+            HeightBar.Value = 4;
+        }
+
+        private void InitializeMap()
+        {
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
+            MapView.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            MapView.MinZoom = 2;
+            MapView.MaxZoom = 16;
+            MapView.Zoom = 4;
+            MapView.Position = new GMap.NET.PointLatLng(66.4169575018027, 94.25025752215694);
+            MapView.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            MapView.CanDragMap = true;
+            MapView.DragButton = MouseButtons.Left;
+            MapView.ShowCenter = false;
+            MapView.ShowTileGridLines = false;
+        }
+
+        private void HeightBar_Scroll(object sender, EventArgs e)
+        {
+            MapView.Zoom = HeightBar.Value;
         }
     }
 }
